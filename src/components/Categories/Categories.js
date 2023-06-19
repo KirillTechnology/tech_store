936fd39911262b449b7d160a43f39792404071ef
Kirill Semenov <kirill.technology@gmail.com>
2023-06-18 17:35:47 -0700

    Categories done

---
+++ src/components/Categories/Categories.js
@@ -0,0 +1,15 @@
+import './categories.scss'
+import Category from '../Category/Category'
+
+function Categories({categories}) {
+
+    return (
+        <div className="categories">
+            {categories.map(x => (
+                <Category key={x.id} category={x} />
+            ))}
+        </div>
+    )
+}
+
+export default Categories
---
+++ src/components/Category/Category.js
@@ -0,0 +1,19 @@
+import './category.scss';
+
+function Category({category}) {
+    const {title, imageUrl} = category;
+
+    return (
+        <div className="category">
+            <div className="category-img" style={{
+                backgroundImage: `url(${imageUrl})`
+            }} />
+            <div className="category-text">
+                <p><b>{title}</b></p>
+                <p>Shop Now</p>
+            </div>
+        </div>
+    )
+}
+
+export default Category
