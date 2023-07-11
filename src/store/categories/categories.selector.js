be4edb61217127514450d6efa1920fbe4f1c9a47
Kirill Semenov <kirill.technology@gmail.com>
2023-07-10 20:54:02 -0700

    Added Reselect to categories.selector.js

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,10 +1,19 @@
-export const selectCategoriesMap = (state) => {
-    console.log('selecCategoriesMap')
-    return (
-        state.categories.categories.reduce((acc, category) => {
+import { createSelector } from "reselect"
+
+const selectCategoriesReducer = (state) => state.categories
+
+export const selectCategories = createSelector(
+    [selectCategoriesReducer],
+    (categoriesSlice) => categoriesSlice.categories
+)
+
+export const selectCategoriesMap = createSelector(
+    [selectCategories],
+    (categories) => {
+        return categories.reduce((acc, category) => {
             const { title, items } = category
             acc[title.toLowerCase()] = items
             return acc
         }, {})
-    )
-}
+    }
+)
