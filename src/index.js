69f1e001a2980dcacb392095936f2e0e484006e3
Kirill Semenov <kirill.technology@gmail.com>
2023-07-11 20:43:15 -0700

    Added Redux-Persist

--- src/index.js
+++ src/index.js
@@ -5,34 +5,35 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
-import { store } from './store/store';
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from './store/store';
 
 import { NotificationProvider } from './context/NotificationContext';
+
 import App from './App';
 import './index.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
+  // <React.StrictMode>
+
+  <Provider store={store}>
+    <PersistGate
+      loading={null}
+      persistor={persistor}
+    >
 
       <BrowserRouter>
         <NotificationProvider>
-
-          {/* <UserProvider> */}
-          {/* <CategoriesProvider> */}
-          {/* <CartProvider> */}
-            <App />
-          {/* </CartProvider> */}
-          {/* </CategoriesProvider> */}
-          {/* </UserProvider> */}
-
+          <App />
         </NotificationProvider>
       </BrowserRouter>
 
-    </Provider>
-  </React.StrictMode>
+    </PersistGate>
+  </Provider>
+
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
